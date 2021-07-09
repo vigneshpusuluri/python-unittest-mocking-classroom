52e65914f8f5ff5ea678faf01e80e8a0928652e7
vigneshpusuluri <vignesh.pusuluri@gmail.com>
2021-07-09 16:01:09 +0530

    updated db_helper.py file

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,19 +1,33 @@
+import mysql.connector
+
 class DbHelper:
+
+    def __init__(self):
+        self.myconnector = mysql.connector.connect(
+            host = 'localhost',
+            user = "root",
+            passwd = "password7",
+            database = "vigneshdg")
+        self.mycursor = self.myconnector.cursor()
+    
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
+
         '''
-        return None
+        self.mycursor.execute('SELECT MAX(emp_no) FROM employees;')
+        return self.mycursor.fetchone()[0]
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+        self.mycursor.execute('SELECT MIN(emp_no) FROM employees;')
+        return self.mycursor.fetchone()[0]
 
-if __name__ == "__main__":
-    db_helper = DbHelper()
-    min_salary = db_helper.get_minimum_salary()
-    max_salary = db_helper.get_maximum_salary()
-    print(max_salary)
-    print(min_salary)
+"""if __name__ == "__main__":
+db_helper = DbHelper()
+min_salary = db_helper.get_minimum_salary()
+max_salary = db_helper.get_maximum_salary()
+print(max_salary)
+print(min_salary)"""
